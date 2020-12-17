27910e0df1ddfe87d7df0200ca36b005032b8e65
SteamB23 <steamb23@outlook.com>
2020-12-17 15:42:55 +0900

    포인트 구조체 비교 연산자 정의

--- GeneticPathFinding/Point.cs
+++ GeneticPathFinding/Point.cs
@@ -14,5 +14,26 @@ namespace GeneticPathFinding
             this.x = x;
             this.y = y;
         }
+
+        public override int GetHashCode()
+        {
+            return (x + y) ^ y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Point point)
+            {
+                return this == point;
+            }
+            return base.Equals(obj);
+        }
+
+        public static bool operator ==(Point v1, Point v2)
+        {
+            return v1.x == v2.x && v1.y == v2.y;
+        }
+
+        public static bool operator !=(Point v1, Point v2) => !(v1 == v2);
     }
 }
